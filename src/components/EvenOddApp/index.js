163c43aa8b840633f9946cac07a43bf80ebfcafd
ramsai24 <jnrsai24@gmail.com>
2023-05-29 13:39:07 +0000

    run has done 2 test cases failed

---
+++ src/components/EvenOddApp/index.js
@@ -0,0 +1,45 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class EvenOddApp extends Component {
+  state = {number: 0, evenOdd: 'Even'}
+
+  oddOrEven = () => {
+    // const {number, evenOdd} = this.state
+
+    this.setState(prevState => {
+      console.log(prevState)
+      // console.log(Math.random() * 100)
+
+      const incrementAndAddition =
+        Math.round(Math.random() * 100) + prevState.number
+      console.log(incrementAndAddition)
+
+      if (incrementAndAddition % 2 === 0) {
+        return {number: incrementAndAddition, evenOdd: 'Even'}
+      }
+      return {number: incrementAndAddition, evenOdd: 'Odd'}
+    })
+  }
+
+  render() {
+    const {number, evenOdd} = this.state
+    return (
+      <div className="bg-container">
+        <div className="even-odd-container">
+          <h1 className="count">
+            Count <span>{number}</span>
+          </h1>
+          <h1 className="evenorodd">Count is {evenOdd}</h1>
+          <button onClick={this.oddOrEven} className="button" type="button">
+            Increment
+          </button>
+          <p className="note">*Increased by random number 0 to 100</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default EvenOddApp
